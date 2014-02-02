d315a72bf153134809d1c25dbcb41d992d2767b2
snaiffer <snaiffer@gmail.com>
2014-02-02 15:17:27 +0400

    Welcome screen was added

--- TreeMind.py
+++ TreeMind.py
@@ -14,11 +14,17 @@ from kivy.uix.textinput import TextInput
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.clock import Clock
 
 from tree import *
 
 Builder.load_file('./TreeMind.kv')
-tx = 'None'
+
+class Add(Button):
+  def on_press(self):
+    print 'on_press Add'
+    return super(Add, self).on_press()
 
 class Branch(Button):
   def on_press(self):
@@ -36,20 +42,32 @@ class MainLayout(GridLayout):
     super(MainLayout, self).__init__(**kwargs)
 
 class ContentLayout(GridLayout):
+  tr = ObjectProperty(None)
+  pass
+
+class WelcomeScreen(Screen):
   pass
 
 class MainScreen(Screen):
-  pass
+  def __init__(self, **kwargs):
+    super(MainScreen, self).__init__(**kwargs)
 
 class LeafScreen(Screen):
   pass
 
 sm = ScreenManager()
+sm.add_widget(WelcomeScreen(name='WelcomeScreen'))
 sm.add_widget(MainScreen(name='MainScreen'))
 sm.add_widget(LeafScreen(name='LeafScreen'))
 
+def showMainScreen(dt):
+  sm.current = 'MainScreen'
+
 class ScrollViewApp(App):
+  gl = 'GLOBAL'
+  tree = Tree()
   def build(self):
+    Clock.schedule_once(showMainScreen)
     return sm
   def on_stop(self):
     pass
