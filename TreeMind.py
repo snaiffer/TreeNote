63ce18e73a1581fa671111c483fc44466e433558
snaiffer <snaiffer@gmail.com>
2014-02-13 14:31:00 +0400

    transitions were changed; AddScreen was added

--- TreeMind.py
+++ TreeMind.py
@@ -10,8 +10,10 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition, FadeTransition , SlideTransition
 from kivy.properties import NumericProperty, ObjectProperty
+from kivy.clock import Clock
+from functools import partial
 
 from tree import *
 
@@ -50,6 +52,7 @@ class ButtonLeaf(ButtonTreeItem):
     self.background_color=[1,1,0,1]
   def on_press(self):
     tree.upTo(self.num)
+    sm.transition = SlideTransition(direction='left')
     sm.current = 'leafScreen'
     return super(ButtonLeaf, self).on_press()
 
@@ -70,6 +73,10 @@ class ContentLayout(GridLayout):
     self.size_hint_y = None
     self.bind(minimum_height=self.setter('height'))  # for scrolling
     self.showTree()
+
+    self.bind(
+      on_touch_down=self.create_clock,
+      on_touch_up=self.delete_clock)
 
   def showTree(self):  
     self.clear_widgets()
@@ -100,6 +107,45 @@ class ContentLayout(GridLayout):
       tree.down()
       self.showTree()
 
+  def create_clock(self, widget, touch, *args):
+    callback = partial(self.menu, touch)
+    Clock.schedule_once(callback, 0.4)
+    touch.ud['event'] = callback
+
+  def delete_clock(self, widget, touch, *args):
+    Clock.unschedule(touch.ud['event'])
+
+  def menu(self, touch, *args):
+    menu = BoxLayout(
+        size_hint=(None, None),
+        orientation='vertical',
+        center=touch.pos)
+    menu.add_widget(Button(text='a'))
+    menu.add_widget(Button(text='b'))
+    close = Button(text='close')
+    close.bind(on_release=partial(self.close_menu, menu))
+    menu.add_widget(close)
+    self.add_widget(menu)
+
+  def close_menu(self, widget, *args):
+    self.remove_widget(widget)
+
+"""
+  def build(self):
+    self.root = FloatLayout()
+    self.root.bind(
+      on_touch_down=self.create_clock,
+      on_touch_up=self.delete_clock)
+"""
+
+class ButtonAdd(Button):
+  def __init__(self, **kwargs):
+    super(ButtonAdd, self).__init__(**kwargs)
+  def on_press(self):
+    sm.transition = FadeTransition() #duration=0.8)
+    sm.current = 'addScreen'
+    return super(ButtonAdd, self).on_press()
+
 class MainLayout(GridLayout):
   def __init__(self, **kwargs):
     super(MainLayout, self).__init__(**kwargs)
@@ -107,14 +153,16 @@ class MainLayout(GridLayout):
 
     # top
     buttonBack = Button(text='Back', size_hint_x=0.1)
-    textField = TextInput(multiline=False, size_hint_x=0.7)
+    textField = TextInput(multiline=False, size_hint_x=0.6)
     buttonAddBranch = Button(text='AddBranch', size_hint_x=0.1)
     buttonAddLeaf = Button(text='AddLeaf', size_hint_x=0.1)
+    buttonAdd = ButtonAdd(text='+', size_hint_x=0.1)
     topLayout = BoxLayout(size_hint_y = 0.1)
     topLayout.add_widget(buttonBack)
     topLayout.add_widget(textField)
     topLayout.add_widget(buttonAddBranch)
     topLayout.add_widget(buttonAddLeaf)
+    topLayout.add_widget(buttonAdd)
     self.add_widget(topLayout) 
     
     # Content
@@ -131,6 +179,7 @@ class ButtonBack_fromLeaf(Button):
     print 'goBack_fromLeaf'
     if not tree.reachRoot():
       tree.down()
+      sm.transition = SlideTransition(direction='right')
       sm.current = 'mainScreen'
 
 class TextField(TextInput):
@@ -161,8 +210,24 @@ class LeafScreen(Screen):
     self.clear_widgets()
     self.add_widget(LeafLayout())
     
+
+class ButtonBack_fromAdd(Button):
+  def on_press(self):
+    sm.transition = FadeTransition()
+    sm.current = 'mainScreen'
+
+class AddLayout(BoxLayout):
+  def __init__(self, **kwargs):
+    super(AddLayout, self).__init__(**kwargs)
+    self.size_hint = (0.7, 0.8)
+    self.orientation = 'vertical'
     
-sm = ScreenManager()
+    self.add_widget(ButtonBack_fromAdd(text='back'))
+    self.add_widget(Button(text='Add Branch'))
+    self.add_widget(Button(text='Add Leaf'))
+    
+
+sm = ScreenManager() 
 
 class ScrollViewApp(App):
   def build(self):
@@ -176,9 +241,14 @@ class ScrollViewApp(App):
 
     leafScreen = LeafScreen(name="leafScreen")
 
+    addScreen = Screen(name='addScreen')
+    addScreen.add_widget(AddLayout())
+
     sm.add_widget(mainScreen)
     sm.add_widget(leafScreen)
+    sm.add_widget(addScreen)
     return sm
+
   def on_stop(self):
     tree.save()
     pass
